b5e8f66e2c8c08e870393e2feb0efae72e10fe00
melsem08 <gersen18@gmail.com>
2023-05-12 05:47:18 +0100

    added some functionality task 7

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const {
   getReviewById,
   getReviews,
   getCommentsByReviewId,
+  postCommentsByReviewId,
 } = require("./controllers/reviews.controller");
 const app = express();
+app.use(express.json());
 
 app.get("/api/categories", getCategories);
 app.get("/api", getApi);
 app.get("/api/reviews/:review_id", getReviewById);
 app.get("/api/reviews", getReviews);
 app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
+app.post("/api/reviews/:review_id/comments", postCommentsByReviewId);
 
 app.use((error, request, response, next) => {
   if (error.code === "22P02") {
--- models/reviews.model.js
+++ models/reviews.model.js
@@ -43,3 +43,28 @@ exports.selectCommentsByReviewId = (reviewId) => {
       return results.rows;
     });
 };
+
+exports.insertCommentsByReviewId = (comment, reviewId) => {
+  if (!comment.hasOwnProperty("username") || !comment.hasOwnProperty("body")) {
+    return Promise.reject({
+      status: 400,
+      message: "Missing necessary property!",
+    });
+  } else if (
+    typeof comment.username !== "string" ||
+    typeof comment.body !== "string"
+  ) {
+    return Promise.reject({
+      status: 400,
+      message: "Incorrect property type!",
+    });
+  }
+  comment.review_id = reviewId;
+  const queryString = `INSERT INTO comments (body, review_id, author) VALUES ($1, $2, $3) RETURNING*;`;
+  const queryValues = [comment.body, comment.review_id, comment.username];
+  return this.selectReviewById(reviewId).then(() => {
+    return db.query(queryString, queryValues).then(({ rows }) => {
+      return rows[0];
+    });
+  });
+};
